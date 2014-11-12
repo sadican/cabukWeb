71fbcef55791f0261a5f1159d5c3b80d01bf9c74
Mahmud Rasih ÇELENLİOĞLU <sadican@users.noreply.github.com>
2014-11-12 21:16:06 +0200

    Update basic_template.py
    change quotes

--- basic_template.py
+++ basic_template.py
@@ -14,7 +14,7 @@ class BasicTemplate:
       + "\n" + '<!-- Bootstrap -->' +
       + "\n" + '<link href="css/bootstrap.min.css" rel="stylesheet">' +
       + "\n" + '<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->' +
-      + "\n" + '<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->' +
+      + "\n" + '<!-- WARNING: Respond.js doesnt work if you view the page via file:// -->' +
       + "\n" + '<!--[if lt IE 9]>' +
       + "\n" + '<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>' +
       + "\n" + '<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>' +
@@ -23,7 +23,7 @@ class BasicTemplate:
       + "\n" + '<body>'
   
   def getFooter(self):
-    return '<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->'
+    return '<!-- jQuery (necessary for Bootstraps JavaScript plugins) -->'
       + "\n" + '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>' +
       + "\n" + '<!-- Include all compiled plugins (below), or include individual files as needed -->' +
       + "\n" + '<script src="js/bootstrap.min.js"></script>' +
