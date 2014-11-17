1b53bedc4b10f7e5c54d81103596160dec70f046
Sadican <sadican@live.com>
2014-11-17 14:42:21 +0200

    update api

--- CWParser.py
+++ CWParser.py
@@ -4,38 +4,41 @@
 from CWFile import CWFile
 from Textbox import Textbox
 
+
 class CWParser:
+    def __init__(self):
+        self.cwFile = CWFile()
+        self.commands = []
+        self.uiObjs = []
 
-	def __init__(self):
-		self.cwFile = CWFile()
-		self.commands = []
-		self.uiObjs = []
+    def parse(self):
+        for command in self.commands:
+            args = command.split(self.cwFile.DELIMITER_COMMAND)
 
-	def parse(self):
-		for command in self.commands:
-			args = command.split(self.cwFile.DELIMITER_COMMAND)
+            if args[0].strip() == Textbox.COMMAND:
+                self.uiObjs.append(Textbox(args[1:len(args)]))
 
-			if args[0].strip() == Textbox.COMMAND:
-				self.uiObjs.append(Textbox(args[1:len(args)]))
+        for obj in self.uiObjs:
+            print
+            obj.getASP()
+            print
+            obj.getHTML()
 
-		for obj in self.uiObjs:
-			print obj.getASP()
-			print obj.getHTML()
-	
-	def readCWContent(self, cwFileName):
-		cwFile = open(cwFileName, "r")
-		
-		self.commands = []
-		for line in cwFile:
-			if line[0] != self.cwFile.COMMENT and line != "" and line != "\n":
-				self.commands.append(line.strip())
-		
-		cwFile.close()
-		return self.commands
+    def read_cw_script(self, file_name):
+        cw_file = open(file_name, "r")
 
-	def showCWContent(self):
-		if self.commands == []:
-			print "no command!"
-		else:
-			for command in self.commands:
-				print command
+        self.commands = []
+        for line in cw_file:
+            if line[0] != self.cwFile.COMMENT and line != "" and line != "\n":
+                self.commands.append(line.strip())
+
+        cw_file.close()
+        return self.commands
+
+    def show_cw_script(self, file_name = ""):
+        if self.commands == []:
+            self.read_cw_script()
+        else:
+            for command in self.commands:
+                print
+                command
--- Textbox.py
+++ Textbox.py
@@ -3,57 +3,64 @@
 
 from CWFile import CWFile
 
+
 class Textbox:
-	COMMAND = "textbox"
-	PREFIX = "tb_"
+    COMMAND = "textbox"
+    PREFIX = "tb_"
 
-	ARG_OBJ_ID = "id"
-	ARG_CSS_CLASS = "cssClass"
-	ARG_IS_REQUIRED = "isRequired"
-	ARG_INPUT_SIZE = "inputSize"
-	ARG_PLACEHOLDER = "placeholder"
+    ARG_OBJ_ID = "id"
+    ARG_CSS_CLASS = "cssClass"
+    ARG_IS_REQUIRED = "isRequired"
+    ARG_INPUT_SIZE = "inputSize"
+    ARG_PLACEHOLDER = "placeholder"
 
-	def __init__ (self, args):
-		self.setDefaults()
-		for arg in args:
-			# split arg key - value
-			tempArg = arg.split(CWFile.DELIMITER_ARG)
-			argName = tempArg[0]
-			argValue = tempArg[1]
+    def __init__(self, args):
+        self.obj_id = self.PREFIX
+        self.css_class = "form-control"
+        self.is_required = False
+        self.input_size = 128
+        self.placeholder = ""
+        # create object
+        self.create(args)
 
-			if argName == self.ARG_OBJ_ID:
-				self.objID = self.PREFIX + argValue
-			elif argName == self.ARG_CSS_CLASS:
-				self.cssClass = argValue
-			elif argName == self.ARG_IS_REQUIRED:
-				self.isRequired = argValue
-			elif argName == self.ARG_INPUT_SIZE:
-				self.inputSize = argValue
-			elif argName == self.ARG_PLACEHOLDER:
-				self.placeholder = argValue
+    def create(self, args):
+        if len(args) < 1:
+            return
+        else:
+            for arg in args:
+                # split arg key - value
+                temp_arg = arg.split(CWFile.DELIMITER_ARG)
+                arg_name = temp_arg[0]
+                arg_value = temp_arg[1]
 
-	def setDefaults(self):
-		self.objID = self.PREFIX
-		self.cssClass = "form-control"
-		self.isRequired = False
-		self.inputSize = 128
-		self.placeholder = ""
+                if arg_name == self.ARG_OBJ_ID:
+                    self.obj_id = self.PREFIX + arg_value
+                elif arg_name == self.ARG_CSS_CLASS:
+                    self.css_class = arg_value
+                elif arg_name == self.ARG_IS_REQUIRED:
+                    self.is_required = arg_value
+                elif arg_name == self.ARG_INPUT_SIZE:
+                    self.input_size = arg_value
+                elif arg_name == self.ARG_PLACEHOLDER:
+                    self.placeholder = arg_value
 
-	def getHTML(self):
-		code = "<input "
-		code += "id=" + "\"" + self.objID + "\" "
-		code += "class=" + "\"" + self.cssClass + "\" "
-		if self.placeholder != "":
-			code += "placeholder=" + "\"" + self.placeholder + "\" "
-		code +=">"
-		return code
+    def get_html(self):
+        code = "<input "
+        code += "id=" + "\"" + self.obj_id + "\" "
+        code += "class=" + "\"" + self.css_class + "\" "
+        if self.placeholder != "":
+            code += "placeholder=" + "\"" + self.placeholder + "\" "
+        code += "maxlength=" + "\"" + self.input_size + "\" "
+        code += ">"
+        return code
 
-	def getASP(self):
-		code = "<asp:TextBox "
-		code += "id=" + "\"" + self.objID + "\" "
-		code += "CssClass=" + "\"" + self.cssClass + "\" "
-		if self.placeholder != "":
-			code += "placeholder=" + "\"" + self.placeholder + "\" "
-		code += "runat=\"server\" "
-		code += "></asp:TextBox>"
-		return code 
+    def get_asp(self):
+        code = "<asp:TextBox "
+        code += "id=" + "\"" + self.obj_id + "\" "
+        code += "CssClass=" + "\"" + self.css_class + "\" "
+        if self.placeholder != "":
+            code += "placeholder=" + "\"" + self.placeholder + "\" "
+        code += "MaxLength=" + "\"" + self.input_size + "\" "
+        code += "runat=\"server\" "
+        code += "></asp:TextBox>"
+        return code
